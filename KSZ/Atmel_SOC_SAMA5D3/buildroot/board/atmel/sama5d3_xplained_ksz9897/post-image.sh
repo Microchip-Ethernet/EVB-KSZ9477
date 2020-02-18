16627e137b2a9885227a2ad20206ff670633828a
Tristram Ha <Tristram.Ha@microchip.com>
2020-02-18 13:11:53 -0800

    Correct build error when using O=<output>.

--- KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME .config | cut -d"\"" -f2)
+DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
 cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
 
--- KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
@@ -4,7 +4,7 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME .config | cut -d"\"" -f2)
+DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
 cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
 install -p -m 644 ${BOARD_DIR}/uboot.env ${BINARIES_DIR}/uboot.env

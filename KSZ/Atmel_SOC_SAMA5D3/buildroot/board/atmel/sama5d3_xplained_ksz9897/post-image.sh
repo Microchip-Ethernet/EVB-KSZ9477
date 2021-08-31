3eab16e7c7d5d4d5ffe86c00d55bdb28d70d8048
Tristram Ha <Tristram.Ha@microchip.com>
2021-08-30 22:13:05 -0700

    Remove build warning when default sama5d3 device tree is used.

--- KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
@@ -2,5 +2,7 @@
 
 DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
+if [ "$DTB" != "at91-sama5d3_xplained" ]; then
 cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+fi
 
--- KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
@@ -6,7 +6,9 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
+if [ "$DTB" != "at91-sama5d3_xplained" ]; then
 cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+fi
 install -p -m 644 ${BOARD_DIR}/uboot.env ${BINARIES_DIR}/uboot.env
 
 rm -rf "${GENIMAGE_TMP}"

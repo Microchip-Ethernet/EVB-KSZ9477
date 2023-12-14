9b8ef41a7f97d7c9dd9b8fad07b3dc9dda43b794
Tristram Ha <Tristram.Ha@microchip.com>
2023-12-14 12:24:59 -0800

    Update KSZ8895 4.9 switch driver.

--- KSZ/linux-drivers/ksz8895/linux-4.9/drivers/net/ethernet/micrel/ksz_cfg_8895.h
+++ KSZ/linux-drivers/ksz8895/linux-4.9/drivers/net/ethernet/micrel/ksz_cfg_8895.h
@@ -7,6 +7,17 @@
 #ifndef KSZ_CFG_8895_H
 #define KSZ_CFG_8895_H
 
+#ifdef CONFIG_KSZ_MRP
+/* MRP code not implemented yet. */
+#undef CONFIG_KSZ_MRP
+#undef CONFIG_KSZ_MMRP
+#undef CONFIG_KSZ_MVRP
+#undef CONFIG_KSZ_MSRP
+#endif
+/* Can be defined if KSZ9897 driver is included also. */
+#undef CONFIG_KSZ_DLR
+#undef CONFIG_KSZ_HSR
+
 
 #include "ksz_common.h"
 

7d26a66d61095695df87fa56ebb9b74bc9cd5ce3
Tristram Ha <Tristram.Ha@microchip.com>
2023-12-14 12:03:53 -0800

    Update switch configuration headers so that all switch drivers can be compiled at the same time without conflict.

--- KSZ/kernels/linux-4.9.143/drivers/net/ethernet/micrel/ksz_cfg_8795.h
+++ KSZ/kernels/linux-4.9.143/drivers/net/ethernet/micrel/ksz_cfg_8795.h
@@ -7,6 +7,16 @@
 #ifndef KSZ_CFG_8795_H
 #define KSZ_CFG_8795_H
 
+#ifdef CONFIG_KSZ_MRP
+/* MRP code not implemented yet. */
+#undef CONFIG_KSZ_MRP
+#undef CONFIG_KSZ_MMRP
+#undef CONFIG_KSZ_MVRP
+#undef CONFIG_KSZ_MSRP
+#endif
+/* Can be defined if KSZ9897 driver is included also. */
+#undef CONFIG_KSZ_HSR
+
 
 #include "ksz_common.h"
 
--- KSZ/kernels/linux-4.9.143/drivers/net/ethernet/micrel/ksz_cfg_8863.h
+++ KSZ/kernels/linux-4.9.143/drivers/net/ethernet/micrel/ksz_cfg_8863.h
@@ -7,6 +7,19 @@
 #ifndef KSZ_CFG_8863_H
 #define KSZ_CFG_8863_H
 
+#ifdef CONFIG_KSZ_MRP
+/* MRP code not implemented yet. */
+#undef CONFIG_KSZ_MRP
+#undef CONFIG_KSZ_MMRP
+#undef CONFIG_KSZ_MVRP
+#undef CONFIG_KSZ_MSRP
+#endif
+/* Can be defined if KSZ9897 driver is included also. */
+#undef CONFIG_KSZ_DLR
+#undef CONFIG_KSZ_HSR
+
+/* KSZ8463 and KSZ8863 use same ksz_sw.c code. */
+#undef CONFIG_HAVE_KSZ8463
 
 #include "ksz_common.h"
 
